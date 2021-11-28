48f74b830aa5b6f496a0c44e9f4a26ed4d298f13
Technocrai <93859604+Technocrai@users.noreply.github.com>
2021-11-28 10:56:05 +0200

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -15,9 +15,9 @@ display.set_caption('Пин Понг')
 background= transform.scale(image.load('Space.jpg'),(700,500))
 
 class GameSprite(sprite.Sprite):
-   def __init__(self,player_image,player_x,player_y,player_speed):
+   def __init__(self,player_image,player_x,player_y,player_speed,w,h):
       super().__init__()
-      self.image = transform.scale(image.load(player_image),(10,80))
+      self.image = transform.scale(image.load(player_image),(w,h))
       self.speed = player_speed
       self.rect = self.image.get_rect()
       self.rect.x = player_x
@@ -25,9 +25,16 @@ class GameSprite(sprite.Sprite):
    def reset(self):
       window.blit(self.image,(self.rect.x, self.rect.y))
 
+class Ball(GameSprite):
+   def __init__(self,player_image,player_x,player_y,player_speed,w,h):
+      super().__init__(player_image,player_x,player_y,player_speed,w,h)
+   def update(self):
+      pass
+
+
 class Player(GameSprite):
-   def __init__(self,player_image,player_x,player_y,player_speed,up,down):
-      super().__init__(player_image,player_x,player_y,player_speed)
+   def __init__(self,player_image,player_x,player_y,player_speed,w,h,up,down):
+      super().__init__(player_image,player_x,player_y,player_speed,w,h)
       self.down=down
       self.up=up
    def update(self):
@@ -37,8 +44,9 @@ class Player(GameSprite):
       if keys_pressed[self.down] and self.rect.y < 470:
          self.rect.y += self.speed
 
-p1=Player('rocket.png',50,250,10,up1,down1)
-p2=Player('rocket.png',600,250,10,up2,down2)
+p1=Player('rocket.png',50,230,10,20,130,up1,down1)
+p2=Player('rocket.png',600,230,10,20,130,up2,down2)
+ball=Ball('Ball.png',330,250,10,60,60)
 
 while game:
    for e in event.get():
@@ -47,6 +55,8 @@ while game:
    window.blit(background,(0, 0))
    p1.update()
    p2.update()
+   ball.update()
+   ball.reset()
    p1.reset()
    p2.reset()
    display.update()
